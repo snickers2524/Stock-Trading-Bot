a700be57f8cdbca7ce9dff3fe7fd0fbdf9e6e384
aidan <aidanvickars@gmail.com>
2021-01-05 13:44:38 -0800

    removed sql.py

--- sql.py
+++
@@ -1,7 +0,0 @@
-config = {
-    'user':'admin',
-    'password':'Watermelon4054',
-    'host':'stock-trader-db.cmppj2geoch9.us-east-1.rds.amazonaws.com',
-    'port':3306,
-    'database':'trades'
-}
